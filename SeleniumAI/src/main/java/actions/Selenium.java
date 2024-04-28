e014978e591a0dd5769ac27327aeef874c0a7463
gmrusso <turangarusso@gmail.com>
2024-04-28 20:00:15 +0200

    Chrome Driver Path selector

--- SeleniumAI/src/main/java/actions/Selenium.java
+++ SeleniumAI/src/main/java/actions/Selenium.java
@@ -1,5 +1,6 @@
 package actions;
 
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
@@ -8,12 +9,17 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
+import factories.MyToolWindowFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.CapabilityType;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 public class Selenium extends DumbAwareAction {
@@ -40,7 +46,9 @@ public class Selenium extends DumbAwareAction {
             logPrefs.enable("webdriver", Level.ALL);
             options.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
 
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ruxo\\Documents\\ChromeDriver\\chromedriver.exe"); // Replace with the path to your ChromeDriver
+            String chromeDriverPath = PropertiesComponent.getInstance().getValue("chromeDriverPath", "C:\\Users\\Ruxo\\Documents\\ChromeDriver\\chromedriver.exe"); // Use the default path if no path is saved
+            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
             WebDriver driver = new ChromeDriver();
             System.out.println("Attempting to open URL: " + url);
             String methodUsed = "";
@@ -48,12 +56,24 @@ public class Selenium extends DumbAwareAction {
                 System.out.println("Driver initialized successfully!");
             }else {
                 System.out.println("Driver initialization failed!");
+                Messages.showMessageDialog(e.getProject(), "Please select the correct chrome driver path", "Error with Driver: ", Messages.getErrorIcon());
+                return;
             }
 
             if(url.startsWith("http://") || url.startsWith("https://")) {
-                driver.get(url);
+                try {
+                    driver.get(url);
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex.getMessage());
+                    Messages.showMessageDialog(e.getProject(), "Please select the correct chrome driver path", "Error with Driver: ", Messages.getErrorIcon());
+                }
             } else {
-                 driver.get("http://" + url);
+                try {
+                    driver.get("http://" + url);
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex.getMessage());
+                    Messages.showMessageDialog(e.getProject(), "Please select the correct chrome driver path", "Error with Driver: ", Messages.getErrorIcon());
+                }
             }
 
             boolean elementExists = false;
@@ -66,6 +86,9 @@ public class Selenium extends DumbAwareAction {
                     } else if(selectedText.startsWith(".")) {
                         methodUsed = "By.cssSelector( "+ selectedText.substring(1) +" )";
                         elementExists = !driver.findElements(By.cssSelector(selectedText)).isEmpty();
+                    } else if(selectedText.startsWith("//") || selectedText.startsWith("./")){
+                        methodUsed = "By.xpath( " + selectedText.substring(1) + " )";
+                        elementExists = !driver.findElements(By.xpath(selectedText)).isEmpty();
                     } else {
                         methodUsed = "By.className( "+ selectedText.substring(1) +" )";
                         elementExists = !driver.findElements(By.className(selectedText)).isEmpty();
@@ -73,12 +96,65 @@ public class Selenium extends DumbAwareAction {
                 }
             } catch (Exception ex) {
                 System.out.println("No elements found with this css");
+                findSimilar(driver, selectedText);
             }
 
+            ArrayList<String> similarElements = new ArrayList<>();
+            similarElements.add("No similar elements found");
+            similarElements.add("No similar elements found2");
+
             Messages.showMessageDialog(e.getProject(), "Method used: " + methodUsed, "Element exists: " + elementExists, Messages.getInformationIcon());
+            System.out.println("finding similar elements...");
+            findSimilar(driver, selectedText);
 
             driver.quit();
         }
+    }
+
+    public void findSimilar(WebDriver driver, String selectedText) {
+        // Find similar elements
+        List<WebElement> allElements = driver.findElements(By.className("*"));
+        ArrayList<String> similarElements = new ArrayList<>();
+        for (WebElement element : allElements) {
+            String elementClass = element.getAttribute("class");
+            if (elementClass != null && (elementClass.contains(selectedText) || calculateMatchPercentage(elementClass, selectedText) > 70)) {
+                similarElements.add(elementClass);
+                System.out.println("New similar element found: " + elementClass);
+            }
+            if(element != null) {
+                if (element.getAttribute("id") != null && (element.getAttribute("id").contains(selectedText) || calculateMatchPercentage(element.getAttribute("id"), selectedText) > 70)) {
+                    similarElements.add(element.getAttribute("id"));
+                }
+                if (element.getAttribute("name") != null && (element.getAttribute("name").contains(selectedText) || calculateMatchPercentage(element.getAttribute("name"), selectedText) > 70)){
+                    similarElements.add(element.getAttribute("name"));
+                }
+                if (element.getAttribute("value") != null && (element.getAttribute("value").contains(selectedText) || calculateMatchPercentage(element.getAttribute("value"), selectedText) > 70)){
+                    similarElements.add(element.getAttribute("value"));
+                }
+                if (element.getAttribute("placeholder") != null && (element.getAttribute("placeholder").contains(selectedText) || calculateMatchPercentage(element.getAttribute("placeholder"), selectedText) > 70)){
+                    similarElements.add(element.getAttribute("placeholder"));
+                }
+            }
+        }
+        // Update the list in the tool window
+        MyToolWindowFactory myToolWindowFactory = new MyToolWindowFactory();
+        myToolWindowFactory.updateSimilarElementsList(similarElements);
+    }
+
+    public static double calculateMatchPercentage(String str1, String str2) {
+        int minLength = Math.min(str1.length(), str2.length());
+        int matchCount = 0;
+
+        for (int i = 0; i < minLength; i++) {
+            if (str1.charAt(i) == str2.charAt(i)) {
+                matchCount++;
+            }
+        }
+
+        if (minLength == 0)
+          return 0;
+
+        return (double) matchCount / minLength * 100;
     }
 }
 // This is an Open Source Project with GPL-3.0 License
--- SeleniumAI/src/main/java/factories/MyToolWindowFactory.java
+++ SeleniumAI/src/main/java/factories/MyToolWindowFactory.java
@@ -1,18 +1,61 @@
 package factories;
 
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.DocumentAdapter;
+import com.intellij.ui.components.JBList;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class MyToolWindowFactory implements ToolWindowFactory {
+    DefaultListModel<String> listModel = new DefaultListModel<>();
+
     @Override
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+        JPanel panel = new JPanel(new BorderLayout());
+        TextFieldWithBrowseButton chromeDriverPathField = new TextFieldWithBrowseButton();
+        chromeDriverPathField.addBrowseFolderListener("Select", null, project,
+                new FileChooserDescriptor(true, false, false, false, false, false));
+
+        chromeDriverPathField.getTextField().getDocument().addDocumentListener(new DocumentAdapter() {
+            @Override
+            protected void textChanged(@NotNull DocumentEvent e) {
+                String chromeDriverPath = chromeDriverPathField.getText();
+                PropertiesComponent.getInstance().setValue("chromeDriverPath", chromeDriverPath);
+            }
+        });
+
+        JPanel smallPanel = new JPanel(new BorderLayout());
+        smallPanel.add(new JLabel("Chrome Driver Path:"), BorderLayout.WEST);
+        smallPanel.add(chromeDriverPathField, BorderLayout.CENTER);
+        panel.add(smallPanel, BorderLayout.NORTH);
+
         ContentFactory contentFactory = ContentFactory.getInstance();
-        Content content = contentFactory.createContent(new JLabel("Selenium Ai Welcome"), "", false);
+        Content content = contentFactory.createContent(panel, "", false);
         toolWindow.getContentManager().addContent(content);
+
+        // Add a JList to display the similar elements
+        JBList<String> similarElementsList = new JBList<>(listModel);
+        panel.add(new JScrollPane(similarElementsList), BorderLayout.CENTER);
+
+    }
+    // Add a method to update the list of similar elements
+    public void updateSimilarElementsList(ArrayList<String> similarElements) {
+        listModel.clear();
+        for (String element : similarElements) {
+            listModel.addElement(element);
+        }
     }
 }
